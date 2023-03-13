a765e33ed6338e749fc43bee2bb10616f6a8858e
Your Name <you@example.com>
2023-03-14 01:13:37 +0700

    tạo mới dự án với Maven

---
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.notebook;
+
+/**
+ *
+ * @author tinho
+ */
+public class NoteBook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
