20b3fc9560e899e36016f4908e54a3f941f8b6ee
NguyenNgocTieuThu <6251071101@st.utc2.edu.vn>
2023-03-14 13:56:49 +0700

    update

--- NoteBook/src/main/java/main/notebook/NoteBook.java
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -10,6 +10,6 @@ public class NoteBook {
     public static void main(String[] args) {
         Home home = new Home();
         home.setVisible(true);
-       
+        System.out.println("hello");
     }
 }
