4a76549b18ad00ba9138db4f209d80674637aa56
Your Name <you@example.com>
2023-03-15 22:35:41 +0700

    update

--- NoteBook/src/main/java/main/notebook/NoteBook.java
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -2,9 +2,14 @@
 
 package main.notebook;
 
+import Views.Home;
+
 public class NoteBook {
 
     public static void main(String[] args) {
+        Home a = new Home();
+        a.setVisible(true);
         
+        System.out.println("hello");
     }
 }
