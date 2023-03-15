49ce06143df28b6ceda1833749cfcf66f6fd19bc
Nhutranh <nhutranh008@gmail.com>
2023-03-15 19:57:49 +0700

    up

--- NoteBook/src/main/java/main/notebook/NoteBook.java
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -2,14 +2,9 @@
 
 package main.notebook;
 
-import Views.tranh;
-
-
 public class NoteBook {
 
     public static void main(String[] args) {
-        tranh a = new tranh();
-        a.setVisible(true);
+        
     }
-
 }
