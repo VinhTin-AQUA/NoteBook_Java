9597901467a0acf38ac721b54fd9283f8f2706be
Your Name <you@example.com>
2023-03-16 22:37:12 +0700

    update lại do có lỗi tào lao

--- NoteBook/src/main/java/main/notebook/NoteBook.java
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -2,18 +2,17 @@
 
 package main.notebook;
 
-import Views.Home;
-
-<<<<<<< HEAD
+import Views.*;
 
 
-=======
->>>>>>> 4a76549b18ad00ba9138db4f209d80674637aa56
 public class NoteBook {
 
     public static void main(String[] args) {
-        Home a = new Home();
-        a.setVisible(true);
+//        Home a = new Home();
+//        a.setVisible(true);
+
+        text text = new text();
+        text.setVisible(true);
         
         System.out.println("hello");
     }
