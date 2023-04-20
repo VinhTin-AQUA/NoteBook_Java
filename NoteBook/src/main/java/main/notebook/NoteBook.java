034eb7b3ed181d9af84026e2a7969cc09e1b4de0
Your Name <you@example.com>
2023-04-20 14:46:29 +0700

    update

--- NoteBook/src/main/java/main/notebook/NoteBook.java
+++ NoteBook/src/main/java/main/notebook/NoteBook.java
@@ -1,21 +1,8 @@
 package main.notebook;
 //
 
-import Components.TodoItem;
 import DataConnection.Data;
 import Views.App;
-import _utility.WrapEditorKit;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 public class NoteBook {
 //
@@ -24,28 +11,6 @@ public class NoteBook {
         Data.loadConnection();
         App a= new App();
         a.setVisible(true);
-//
-//        test t = new test();
-//        t.setVisible(true);
-
-//        JFrame frame = new JFrame("Testing");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        JTextArea field = new JTextArea(1, 30);
-//        field.setLineWrap(true);
-//        field.setWrapStyleWord(true);
-//
-//        GridBagConstraints gbc = new GridBagConstraints();
-//        gbc.weightx = 1;
-//        gbc.fill = GridBagConstraints.HORIZONTAL;
-//
-//        TestPane pane = new TestPane();
-//        pane.add(field, gbc);
-//
-//        frame.add(pane);
-//        frame.pack();
-//        frame.setLocationRelativeTo(null);
-//        frame.setVisible(true);
     }
 }
 
